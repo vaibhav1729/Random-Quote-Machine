8ec60065f7d0fc0ace7a63aed0b6306ec43997b8
arosamiran <arosamiran@outlook.ph>
2017-11-09 12:20:42 +0800

    included the boilerplate for fetch() method - get

--- generate.js
+++ generate.js
@@ -1,9 +1,14 @@
-fetch('http://api.forismatic.com/api/1.0/').then(response => {
-  if (response.ok){
-    return response.json();
-  }
+let crossOrigin = "https://crossorigin.me";
+let http = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en'; 
 
-  throw new Error('Request failed!');
-}, networkError => {
-  console.log(networkError.message);
-}).then(jsonResponse => jsonResponse)
+function getQuote() {
+  fetch(crossOrigin + http).then(response => {
+    if (response.ok){
+      console.log(response.json());
+    }
+  
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  }).then(jsonResponse => jsonResponse)  
+}
