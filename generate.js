5ffa48021a4676e897d404d70313f76faacb187c
arosamiran <arosamiran@outlook.com>
2017-11-09 22:12:29 +0800

    working (hopefully)

--- generate.js
+++ generate.js
@@ -1,5 +1,3 @@
-function getQuote(){
-  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=&callback=", function (a) {
     $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
   });
-}
