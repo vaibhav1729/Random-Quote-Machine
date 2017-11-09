d2f5de97e91ec4d112129eea953a17fb459d965a
arosamiran <arosamiran@outlook.ph>
2017-11-09 12:55:57 +0800

    used #quote instead of body tag

--- generate.js
+++ generate.js
@@ -1,3 +1,3 @@
 $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-  $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+  $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
 });
