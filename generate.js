0699b09f84c5dbe59e6c7f7f3701e24053c6a770
arosamiran <arosamiran@outlook.ph>
2017-11-09 12:57:17 +0800

    wraps inside a function

--- generate.js
+++ generate.js
@@ -1,3 +1,5 @@
-$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-  $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-});
+function getQuote(){
+  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+  });
+}
