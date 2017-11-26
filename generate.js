db4a54146c4fea6cd8601b9492b5f5ddfdc658a9
arosamiran <arosamiran@outlook.com>
2017-11-26 13:49:34 +0800

    bootstrap 4 beta

--- generate.js
+++ generate.js
@@ -1,3 +1,7 @@
-  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=&callback=", function (a) {
+let crossOrigin = "https://crossorigin.me";
+let quotesOnDesign = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=&callback=";
+let url = crossOrigin + quotesOnDesign;
+
+function getQuote(){$.getJSON(url, function (a) {
     $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-  });
+  });}
